3315720cf6c8a2ac7c538329ab51429bc87c8fba
Rima Falko <rima@falko.email>
2026-05-04 01:35:22 +0200

    add generic type generation examples, for show usage samples

---
+++ Examples/Falko.Foundry.Examples.GenericType/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Falko.Foundry.CSharp.Elements;
+using Falko.Foundry.CSharp.Languages;
+using Falko.Foundry.Languages;
+
+var serviceType = new TypeElement
+{
+    Name = "Service"u8
+};
+
+var loggerType = new TypeElement
+{
+    Namespace = "Falko.Logging.Loggers"u8,
+    Name = "Logger"u8,
+    GenericTypes = serviceType
+};
+
+var result = CSharpLanguageCompiler
+    .Instance
+    .CompileElement(in loggerType)
+    .ToString();
+
+Console.WriteLine(result);
+
+Debug.Assert(result is "Falko.Logging.Loggers.Logger<Service>");
