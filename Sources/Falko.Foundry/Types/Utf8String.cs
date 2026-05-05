8d89969933e15d8bd164fd64b72f22eb692a3565
Rima Falko <rima@falko.email>
2026-05-05 18:48:44 +0200

    rename parameter u8-bytes to utf8-bytes for consistency

--- Sources/Falko.Foundry/Types/Utf8String.cs
+++ Sources/Falko.Foundry/Types/Utf8String.cs
@@ -7,7 +7,7 @@ namespace Falko.Foundry.Types;
 public readonly struct Utf8String(ReadOnlyMemory<byte> utf8Bytes)
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public Utf8String(ReadOnlySpan<byte> u8Bytes) : this(new ReadOnlyMemory<byte>(u8Bytes.ToArray())) { }
+    public Utf8String(ReadOnlySpan<byte> utf8Bytes) : this(new ReadOnlyMemory<byte>(utf8Bytes.ToArray())) { }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public Utf8String(string text) : this(Encoding.UTF8.GetBytes(text)) { }
@@ -34,10 +34,10 @@ public readonly struct Utf8String(ReadOnlyMemory<byte> utf8Bytes)
     public override string ToString() => Encoding.UTF8.GetString(utf8Bytes.Span);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static implicit operator Utf8String(ReadOnlyMemory<byte> u8Bytes) => new(u8Bytes);
+    public static implicit operator Utf8String(ReadOnlyMemory<byte> utf8Bytes) => new(utf8Bytes);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static implicit operator Utf8String(ReadOnlySpan<byte> u8Bytes) => new(u8Bytes);
+    public static implicit operator Utf8String(ReadOnlySpan<byte> utf8Bytes) => new(utf8Bytes);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static implicit operator Utf8String(string text) => new(text);
