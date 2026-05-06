0ac5dd28e48bc066369fbbbaf180ef28e64b180c
Rima Falko <rima@falko.email>
2026-05-06 03:10:43 +0200

    add foreach benchmarks for utf8-string

---
+++ Benchmarks/Falko.Foundry.Benchmarks.StringVsUtf8String/ForeachStringBenchmark.cs
@@ -0,0 +1,56 @@
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using Falko.Foundry.Types;
+
+namespace Falko.Benchmarks;
+
+[MemoryDiagnoser]
+public class ForeachStringBenchmark
+{
+    private string _textValue = null!;
+    private Utf8String _utf8TextValue = Utf8String.Empty;
+
+    [Params(1, 3)]
+    public int Iterations { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        var textValue = BenchmarkStringFactory.CreateString();
+
+        _textValue = textValue;
+        _utf8TextValue = textValue.ToUtf8String();
+    }
+
+    [Benchmark(Baseline = true)]
+    public char ForeachString()
+    {
+        var result = char.MinValue;
+
+        for (var i = 0; i < Iterations; i++)
+        {
+            foreach (var c in _textValue)
+            {
+                result = c;
+            }
+        }
+
+        return result;
+    }
+
+    [Benchmark]
+    public Rune ForeachUtf8String()
+    {
+        var result = Rune.ReplacementChar;
+
+        for (var i = 0; i < Iterations; i++)
+        {
+            foreach (var c in _utf8TextValue)
+            {
+                result = c;
+            }
+        }
+
+        return result;
+    }
+}
--- Benchmarks/Falko.Foundry.Benchmarks.StringVsUtf8String/Program.cs
+++ Benchmarks/Falko.Foundry.Benchmarks.StringVsUtf8String/Program.cs
@@ -4,3 +4,4 @@ using Falko.Benchmarks;
 BenchmarkRunner.Run<CreateStringBenchmark>();
 BenchmarkRunner.Run<ConvertStringBenchmark>();
 BenchmarkRunner.Run<AppendStringBenchmark>();
+BenchmarkRunner.Run<ForeachStringBenchmark>();
