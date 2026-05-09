aad1e4f609702df973ef5a75804416a7d3badb65
Rima Falko <rima@falko.email>
2026-05-09 17:21:39 +0200

    reduce files numbers

--- Sources/Falko.Foundry/Utf8Texts/Utf8StringExtensions.cs
+++ Sources/Falko.Foundry/Utf8Texts/Utf8StringExtensions.cs
@@ -6,5 +6,11 @@ namespace Falko.Foundry.Utf8Texts;
 public static class Utf8StringExtensions
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Utf8String ToUtf8String(this string value) => new(value);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Utf8String ToUtf8String(this ReadOnlySpan<byte> value) => new(value);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static int Count(this Utf8String value) => Encoding.UTF8.GetCharCount(value);
 }
